7db1b58744c3fed40888075ba650a3fc3715f53b
Anton <a.zhuravlev58@gmail.com>
2020-09-24 17:45:36 +0300

    2com

--- calc.sh
+++ calc.sh
@@ -1,16 +1,51 @@
 #!/bin/bash
 
-case "$1" in
-"sum")
-echo "$(($2 + $3))";;
+isnum()
+{
+ #re='^[0-9]+$'
+  if [ -n "$1" ] && ["$1" -eq "$1" ] 2>/dev/null; then
+   return 0
+  else
+   return 1
+  fi
+}
 
-"sub")
-echo "$(($2 - $3))";;
+calc()
+{
+ call_math=$1
+ arg1=$2
+ arg2=$3
 
-"mul")
-echo "$(($2 * $3))";;
+ if ! $(isnum $arg1); then
+  echo -e "${RED}First argument is not number ${NC}" 1>&2
+  return 1
+ fi
+ if ! $(isnum $arg2); then
+  echo -e "${RED}Second argument is not number${NC}" 1>&2
+  return 1
+ fi
 
-"div")
-echo "$(($2 / $3))";;
 
+case $call_math in
+ mul)
+  echo "RESULT: $(( arg * arg2 ))"
+  ;;
+ div)
+  if (($arg2 ==0 )); then
+   echo -e "${RED}DIV ZERO${NC}" 1>&2
+   return 1
+  fi
+  echo "RESULT: $(( arg1 / arg2))"
+  ;;
+ sum)
+  echo "RESULT: $(( arg1 + arg2 ))"
+  ;;
+ sub)
+  echo "RESULT: $(( arg1 - arg2 ))"
+  ;;
+ *)
+  echo -e "${RED}CALC FUNCTION ERROR${NC}" 1>&2
+  return -1
+  ;;
 esac
+}
